d62814f0fc0c974ce9a9ad1d3e0e1d1c0bbc3531
DILLON DAVIS <ddavis126@cnm.edu>
2024-05-22 18:59:22 -0600

    Add project files.

---
+++ ClassExamples22MAY2024/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ClassExamples22MAY2024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ ClassExamples22MAY2024/Student.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassExamples22MAY2024
+{
+    internal class Student
+    {
+    }
+}
