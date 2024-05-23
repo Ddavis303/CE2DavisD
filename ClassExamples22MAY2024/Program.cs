5c5f47afdf70eb1639cf44f200ae2404a0930e63
DILLON DAVIS <ddavis126@cnm.edu>
2024-05-22 21:26:41 -0600

    Created Student class

--- ClassExamples22MAY2024/Program.cs
+++ ClassExamples22MAY2024/Program.cs
@@ -1,10 +1,49 @@
-﻿namespace ClassExamples22MAY2024
+﻿//Dillon Davis
+//22MAY2024 ddavis126@cnm.edu
+//Class Example, Classes
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassExamples22MAY2024
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Student student1 = new Student();
+            Student student2 = new Student();
+            Student[] students = new Student[5]; 
+
+            for(int i = 0; i < students.Length; i++) 
+            {
+                students[i] = new Student();            
+            }
+
+            //unable to access directly because they are now set to private
+            //student1._firstName = "Joe";
+            //student2._lastName = "Smith";
+
+            student1.SetFirstName("Bob");
+            students[0] = student1;
+            //students[0].SetAge(25);
+            Console.WriteLine(students[0].GetFirstName());
+            students[0].SetAge();
+
+            foreach(Student element in students)
+            {
+                if(element.GetFirstName() == null)
+                {
+                    //do nothing
+                }
+                else
+                {
+                    Console.WriteLine(element.GetFirstName());
+                }
+            }
         }
     }
 }
--- ClassExamples22MAY2024/Student.cs
+++ ClassExamples22MAY2024/Student.cs
@@ -8,5 +8,50 @@ namespace ClassExamples22MAY2024
 {
     internal class Student
     {
+        private string _firstName;
+        private string _lastName;
+        private int _age;
+
+        //mutators
+        #region Mutators
+        public void SetFirstName(string firstName)
+        {
+            _firstName = firstName;
+        }
+        public void SetAge(int age) 
+        {
+            _age = age;
+        }
+
+        public void SetAge()
+        {
+            int userInput = -1;
+
+            try
+            {
+                Console.Write("Please enter the students age (ie 24): ");
+                userInput = int.Parse(Console.ReadLine());
+                _age = userInput;
+            }
+            catch (FormatException exc)
+            {
+                Console.WriteLine(exc.Message);
+            }
+            catch (Exception exc) 
+            {
+                Console.WriteLine(exc.StackTrace);
+            }
+
+        }
+        #endregion
+
+        //accessors 
+        #region Accessors
+        public string GetFirstName() 
+        {
+            return _firstName;
+        }
+        #endregion
+
     }
 }
